916ed1c2aeb0c0fe27d4b25c5990b9a4bb6acefd
J.A. Ramirez <jaramirez1729@gmail.com>
2021-12-24 00:40:05 -0600

    Finish tree data structures for Leet Code.

--- LeetCode-Algorithms/TreesAlgorithms/TreesAlgorithms/Algorithms.swift
+++ LeetCode-Algorithms/TreesAlgorithms/TreesAlgorithms/Algorithms.swift
@@ -13,6 +13,177 @@ import Foundation
 
  A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
  */
+// We can use recursion and a dictionary. The dictionary key will contain the level of depth. Since keys cannot have duplicates, it's good to use with recursion.
+// O(N), O(N)
 func maxDepth(_ root: TreeNode?) -> Int {
+    var depth: [Int: Int] = [:]
+    findMaxDepth(of: root, depth: &depth, count: 1)
+    return depth.keys.max() ?? 0
+}
+
+func findMaxDepth(of node: TreeNode?, depth: inout [Int: Int], count: Int) {
+    guard node != nil else { return }
+    depth[count] = 0
+    findMaxDepth(of: node?.left, depth: &depth, count: count + 1)
+    findMaxDepth(of: node?.right, depth: &depth, count: count + 1)
+}
+
+// You can avoid using a dictionary by simply adding to the returned value of the original function but this is slower.
+// O(N), O(N)
+//func maxDepth(_ root: TreeNode?) -> Int {
+//    guard root != nil else { return 0 }
+//    let lhs = maxDepth(root?.left) + 1
+//    let rhs = maxDepth(root?.right) + 1
+//    return max(lhs, rhs)
+//}
+
+// MARK: - Validate Binary Search Tree
+/*
+ Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+ A valid BST is defined as follows:
+
+ The left subtree of a node contains only nodes with keys less than the node's key.
+ The right subtree of a node contains only nodes with keys greater than the node's key.
+ Both the left and right subtrees must also be binary search trees.
+ */
+// Learn, Example, Brute, Optimize (BUD), Walkthrough, Implement, Test
+//                  10
+//          8                15
+//       7     9         11     16
+// We can compare the parent value with the left and right child with in-order traversal. This is because there is no need to go deeper if it's invalid at the current level. Only if it's true that the condition is met, do we go further. We also need to consider the previous value since the BST must satisfy at each level.
+// 
+func isValidBST(_ root: TreeNode?) -> Bool {
+    var lastValue: Int?
+    return verifyBST(root, lastValue: &lastValue)
+}
+
+func verifyBST(_ root: TreeNode?, lastValue: inout Int?) -> Bool {
+    guard let current = root else { return true }
     
+    // Since in-order traversal goes to the left most node first, we check that the
+    // last value is greater than the current value, if available.
+    if let prevValue = lastValue, prevValue >= current.val { return false }
+    // Go left first.
+    if !verifyBST(current.left, lastValue: &lastValue) { return false }
+    // Compare and visit self.
+    if let leftValue = lastValue, leftValue >= current.val { return false }
+    lastValue = current.val
+    // Go right.
+    if !verifyBST(current.right, lastValue: &lastValue) { return false }
+    
+    return true
+}
+
+// MARK: - Symmetric Tree
+/*
+ Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ */
+// Approach #1: We can traverse the left tree in-order which goes left to right. The right tee is also in-order but beginning right to left. This ensures checking a mirror version of the tree.
+//                  10
+//          8                8
+//       7     9         9      7
+// O(N), O(N)
+func isSymmetric(_ root: TreeNode?) -> Bool {
+    var leftValues = [Int?]()
+    var rightValues = [Int?]()
+    traverseInOrder(root?.left, values: &leftValues, isLeft: true)
+    traverseInOrder(root?.right, values: &rightValues)
+    return leftValues == rightValues
+}
+
+func traverseInOrder(_ root: TreeNode?, values: inout [Int?], isLeft: Bool = false) {
+    if let val = root?.val {
+        values.append(val)
+        if isLeft {
+            traverseInOrder(root?.left, values: &values, isLeft: true)
+            traverseInOrder(root?.right, values: &values, isLeft: true)                
+        } else {
+            traverseInOrder(root?.right, values: &values)    
+            traverseInOrder(root?.left, values: &values)
+        }
+    } else {
+        values.append(nil)
+    }
+}
+
+// MARK: - Level Order Traversal
+/*
+ Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+ */
+// Learn, Example, Brute, Optimize, Walkthrough, Implement, Test 
+//                  10
+//          8                15
+//       7     9         11     16
+// Approach #1: Use a mutable dictionary that will be passed into the methods by reference and record a new array of the node's at each level and eventually return the results at the end. Since keys cannot be overwritten, the level will be mantained by the dictionary.
+// list: [1: [10], 2: [8, 15], 3: [7, 9], ...]
+// O(N), O(N)
+func levelOrderBrute(_ root: TreeNode?) -> [[Int]] {
+    var list: [Int: [Int]] = [:]
+    if let val = root?.val { list[1] = [val] }
+    recordLevelOrder(root, list: &list, level: 2)
+    
+    var nodes: [[Int]] = []
+    guard !list.isEmpty else { return [] }
+    for i in 1...list.count {   // O(n)
+        if let values = list[i] {
+            nodes.append(values)
+        }
+    }
+    
+    return nodes
+}
+
+func recordLevelOrder(_ root: TreeNode?, list: inout [Int: [Int]], level: Int) {
+    guard let root = root else { return }
+    
+    var values: [Int] = list[level] ?? [Int]()
+    if let leftVal = root.left?.val {
+        values.append(leftVal)
+    }
+    if let rightVal = root.right?.val {
+        values.append(rightVal)
+    }
+    if values.isEmpty { return }
+    list[level] = values
+    
+    recordLevelOrder(root.left, list: &list, level: level + 1)
+    recordLevelOrder(root.right, list: &list, level: level + 1)
+}
+
+// Approach #2: We can avoid recursion by using a queue. We will add the root to the queue. While the queue is not empty, we will dequeue the first element, record its value, and than add the node's left and right child to the queue.
+//           3
+//     9         20
+//           15      7
+// Queue: [20]
+// Level: 0, 1
+// Levels: [[3], [9]]
+// Level Length: 2, 
+// Dequeued: 3, 9
+func levelOrder(_ root: TreeNode?) -> [[Int]] {
+    guard let root = root else { return [] }
+    
+    var queue: [TreeNode?] = [root]    
+    var levels: [[Int]] = []
+    var level: Int = 0
+    
+    while !queue.isEmpty {
+        // Start the current level list.
+        levels.append([Int]())
+        
+        // The number of elements in the current level.
+        let levelLength = queue.count
+        for _ in 0..<levelLength {
+            if let node = queue.removeFirst() {
+                levels[level].append(node.val)
+                
+                // Add the children of the current level into the queue for the next level.
+                if node.left != nil { queue.append(node.left!) }
+                if node.right != nil { queue.append(node.right!) }
+            }
+        }
+        level += 1
+    }
+    
+    return levels
 }
--- LeetCode-Algorithms/TreesAlgorithms/TreesAlgorithmsTests/TreesAlgorithmsTests.swift
+++ LeetCode-Algorithms/TreesAlgorithms/TreesAlgorithmsTests/TreesAlgorithmsTests.swift
@@ -8,26 +8,175 @@
 import XCTest
 @testable import TreesAlgorithms
 
-class TreesAlgorithmsTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+// MARK: - Maximum Depth of Binary Tree
+class MaxDepthTests: XCTestCase {
+    
+    func test1() {
+        let root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+        let result = maxDepth(root)
+        XCTAssertEqual(result, 3)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test2() {
+        let root = TreeNode(1, TreeNode(2), TreeNode(3))
+        let result = maxDepth(root)
+        XCTAssertEqual(result, 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test3() {
+        let root = TreeNode(1)
+        let result = maxDepth(root)
+        XCTAssertEqual(result, 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test4() {
+        let result = maxDepth(nil)
+        XCTAssertEqual(result, 0)
     }
+    
+    func test5() {
+        let root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7, nil, TreeNode(5))))
+        let result = maxDepth(root)
+        XCTAssertEqual(result, 4)
+    }
+}
 
+// MARK: - Validate Binary Search Tree
+class IsValidBSTTests: XCTestCase {
+    func test1() {
+        let root = TreeNode(2, TreeNode(1), TreeNode(3))
+        let result = isValidBST(root)
+        XCTAssertTrue(result)
+    }
+    
+    func test2() {
+        let root = TreeNode(5, TreeNode(1), TreeNode(3))
+        let result = isValidBST(root)
+        XCTAssertFalse(result)
+    }
+    
+    func test3() {
+        let leftTree = TreeNode(8, TreeNode(7), TreeNode(9))
+        let rightTree = TreeNode(15, TreeNode(11), TreeNode(16))
+        let root = TreeNode(10)
+        root.left = leftTree
+        root.right = rightTree
+        let result = isValidBST(root)
+        XCTAssertTrue(result)
+    }
+    
+    func test4() {
+        let rightTree = TreeNode(6, TreeNode(3), TreeNode(7))
+        let root = TreeNode(5, TreeNode(4), rightTree)
+        root.right = rightTree
+        let result = isValidBST(root)
+        XCTAssertFalse(result)
+    }   
+    
+    func test5() {
+        let root = TreeNode(2, TreeNode(2), TreeNode(2))
+        let result = isValidBST(root)
+        XCTAssertFalse(result)
+    }
+    
+    func test6() {
+        let root = TreeNode(2, TreeNode(2), TreeNode(5))
+        let result = isValidBST(root)
+        XCTAssertFalse(result)
+    }
+}
+
+// MARK: - Is Symmetric
+class IsSymmetricTests: XCTestCase {
+    func test1() {
+        let leftTree = TreeNode(8, TreeNode(7), TreeNode(9))
+        let rightTree = TreeNode(8, TreeNode(9), TreeNode(7))
+        let root = TreeNode(10)
+        root.left = leftTree
+        root.right = rightTree
+        let result = isSymmetric(root)
+        XCTAssertTrue(result)
+    }
+    
+    func test2() {
+        let root = TreeNode(2, TreeNode(2), TreeNode(2))
+        let result = isSymmetric(root)
+        XCTAssertTrue(result)
+    }
+    
+    func test3() {
+        let root = TreeNode(2, TreeNode(2), TreeNode(5))
+        let result = isSymmetric(root)
+        XCTAssertFalse(result)
+    }
+    
+    func test4() {
+        let leftTree = TreeNode(8, TreeNode(7), TreeNode(9))
+        let rightTree = TreeNode(15, TreeNode(11), TreeNode(16))
+        let root = TreeNode(10)
+        root.left = leftTree
+        root.right = rightTree
+        let result = isSymmetric(root)
+        XCTAssertFalse(result)
+    }
+    
+    func test5() {
+        let rightTree = TreeNode(2, TreeNode(2), nil)
+        let root = TreeNode(1, TreeNode(2, TreeNode(2), nil), rightTree)
+        let result = isSymmetric(root)
+        XCTAssertFalse(result)
+    }
+}
+
+// MARK: - Level Order Traversal
+class levelOrderTests: XCTestCase {
+    func test1() {
+        //                  10
+        //          8                15
+        //       7     9         11     16
+        let leftTree = TreeNode(8, TreeNode(7), TreeNode(9))
+        let rightTree = TreeNode(15, TreeNode(11), TreeNode(16))
+        let root = TreeNode(10)
+        root.left = leftTree
+        root.right = rightTree
+        let result = levelOrder(root)
+        XCTAssertEqual(result, [[10], [8, 15], [7, 9, 11, 16]])
+    }
+    
+    func test2() {
+        //                  10
+        //          8               8
+        //      7       9       9       7
+        let leftTree = TreeNode(8, TreeNode(7), TreeNode(9))
+        let rightTree = TreeNode(8, TreeNode(9), TreeNode(7))
+        let root = TreeNode(10)
+        root.left = leftTree
+        root.right = rightTree
+        let result = levelOrder(root)
+        XCTAssertEqual(result, [[10], [8, 8], [7, 9, 9, 7]])
+    }
+    
+    func test3() {
+        //           3
+        //     9         20
+        //           15      7
+        let leftTree = TreeNode(9)
+        let rightTree = TreeNode(20, TreeNode(15), TreeNode(7))
+        let root = TreeNode(3)
+        root.left = leftTree
+        root.right = rightTree
+        let result = levelOrder(root)
+        XCTAssertEqual(result, [[3], [9, 20], [15, 7]])
+    }
+    
+    func test4() {
+        let root = TreeNode(1)
+        let result = levelOrder(root)
+        XCTAssertEqual(result, [[1]])
+    }
+    
+    func test5() {
+        let result = levelOrder(nil)
+        XCTAssertEqual(result, [])
+    }
 }
