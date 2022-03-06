094a91a0ff40200a51b1a0c97bdb056dc9d6f863
J.A. Ramirez <jaramirez1729@gmail.com>
2022-03-05 22:26:59 -0600

    Add Two Pointer files.

--- LeetCode-Algorithms/DesignPatterns/DesignPatterns/TwoPointersAlgorithms.swift
+++ LeetCode-Algorithms/DesignPatterns/DesignPatterns/TwoPointersAlgorithms.swift
@@ -17,7 +17,33 @@ import Foundation
 
  Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
  */
+// We can use the 2 pointers approach. The first pointer will contain the index of the duplicate number in order to replace it later. The 2nd pointer will go down the list as normal to check each number. The algorithm will keep moving non-duplicate numbers to the right order. This is essentially replacing the duplicate number. When it's done, the duplicateIndex contains the position of the last valid number moved.
+// Time O(N), Space O(1)
+// ~30 min
 func removeDuplicates(_ nums: inout [Int]) -> Int {
+    guard !nums.isEmpty else { return 0 }
+    var duplicateIndex = 1 // The first pointer.
+    for i in 1..<nums.endIndex { // The second pointer.
+        // The (duplicateIndex - 1) position is the last valid number in the sequence. If the ith number is not a copy, then we will move the ith number right after it, at the duplicateIndex position. Then we move the first pointer ahead.
+        if nums[duplicateIndex - 1] != nums[i] {
+            nums[duplicateIndex] = nums[i] // Move the valid ith number to the right spot.
+            duplicateIndex += 1
+        }
+    }
+    return duplicateIndex
+}
+
+// MARK: - Remove Element (Easy)
+/*
+ Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+ Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+ Return k after placing the final result in the first k slots of nums.
+
+ Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+ */
+func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
     
     return 0
 }
--- LeetCode-Algorithms/DesignPatterns/DesignPatternsTests/TwoPointersAlgorithmsTests.swift
+++ LeetCode-Algorithms/DesignPatterns/DesignPatternsTests/TwoPointersAlgorithmsTests.swift
@@ -6,8 +6,29 @@
 //
 
 import XCTest
+@testable import DesignPatterns
 
 // MARK: - Remove Duplicates from Sorted Array (Easy)
 class RemoveDuplicatesTests: XCTestCase {
+    func test1() {
+        var nums = [1, 2, 3, 3, 4, 5, 5, 6]
+        XCTAssertEqual(removeDuplicates(&nums), 6)
+    }
     
+    func test2() {
+        var nums = [1, 1, 2]
+        XCTAssertEqual(removeDuplicates(&nums), 2)
+    }
+    
+    func test3() {
+        var nums = [0, 0, 1, 1, 1 ,2, 2, 3, 3, 4]
+        XCTAssertEqual(removeDuplicates(&nums), 5)
+    }
+}
+
+// MARK: - Remove Element (Easy)
+class RemoveElementTests: XCTestCase {
+    func test1() {
+        
+    }
 }
