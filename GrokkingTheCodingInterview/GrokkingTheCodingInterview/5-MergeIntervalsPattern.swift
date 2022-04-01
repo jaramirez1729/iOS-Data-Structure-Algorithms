522f6bf19671db9b1a51dd687eebfc3276d05423
J.A. Ramirez <jaramirez1729@gmail.com>
2022-03-31 23:34:09 -0500

    Complete an interval question.

--- GrokkingTheCodingInterview/GrokkingTheCodingInterview/5-MergeIntervalsPattern.swift
+++ GrokkingTheCodingInterview/GrokkingTheCodingInterview/5-MergeIntervalsPattern.swift
@@ -32,14 +32,14 @@ func mergeIntervals(_ intervals: [Interval]) -> [Interval] {
     let sortedIntervals = intervals.sorted(by: { $0.start < $1.start })
     var mergedIntervals = [Interval]()
     
-    // Start off at 0 to begin the comparisons in the loop.
+    // Get the start and end of the first interval. This will be used to compare the next intervals. They will also be used for subsequent overlapping intervals.
     var start = sortedIntervals[0].start
     var end = sortedIntervals[0].end
     for i in 1..<sortedIntervals.endIndex {
         let interval = sortedIntervals[i]
         if interval.start <= end { // Overlapping intervals, adjust the end.
             end = max(interval.end, end)
-        } else { // non-overlapping interval, add the previous interval and reset.
+        } else { // non-overlapping interval, add the previous interval that did overlap before we re-assign them.
             mergedIntervals.append(Interval(start, end))
             start = interval.start
             end = interval.end
@@ -50,10 +50,64 @@ func mergeIntervals(_ intervals: [Interval]) -> [Interval] {
     return mergedIntervals
 } 
 
-// MARK: - Merge Intervals (Medium)
+// MARK: Similar Problem #1
+/*
+ Given a set of intervals, find out if any two intervals overlap.
+ */
+// Time O(N * log N), Space O(N)
+func doIntervalsOverlap(_ intervals: [Interval]) -> Bool {
+    guard !intervals.isEmpty else { return false }
+    
+    let sortedIntervals = intervals.sorted(by: { $0.start < $1.start })
+    var end = sortedIntervals[0].end
+    
+    for i in 1..<sortedIntervals.endIndex {
+        let interval = sortedIntervals[i]
+        if interval.start <= end {
+            return true
+        } else {
+            // Update the end interval to use it for comparison with the next value.
+            end = interval.end
+        }
+    }
+    return false
+}
+
+// MARK: - Insert Intervals (Medium)
 /*
  Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
  */
-func instertInterval(_ interval: Interval, into intervals: [Interval]) -> [Interval] {
+// Time O(N), Space O(N)
+func insertInterval(_ interval: Interval, into intervals: [Interval]) -> [Interval] {
+    var mergedIntervals = [Interval]()
+    var i = 0
+
+    // Since intervals don't overlap, we can add all that come before the new one already.
+    while i < intervals.count && intervals[i].end < interval.start {
+        mergedIntervals.append(intervals[i])
+        i += 1
+    }
+    // Create a new interval that includes any intervals that overlap with the new one.
+    let newInterval = interval
+    while i < intervals.count && intervals[i].start <= newInterval.end {
+        newInterval.start = min(intervals[i].start, newInterval.start)
+        newInterval.end = max(intervals[i].end, newInterval.end)
+        i += 1
+    }
+    mergedIntervals.append(newInterval)
+    // If there are any left, then they are already not overlapping, so just add the rest.
+    while i < intervals.count {
+        mergedIntervals.append(intervals[i])
+        i += 1
+    }
+    
+    return mergedIntervals
+}
+
+// MARK: - Intervals Intersection (Medium)
+/*
+ Given two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
+ */
+func findIntersection(_ list1: [Interval], _ list2: [Interval]) -> [Interval] {
     return []
 }
--- GrokkingTheCodingInterview/GrokkingTheCodingInterviewTests/5-MergeIntervalsPatternTests.swift
+++ GrokkingTheCodingInterview/GrokkingTheCodingInterviewTests/5-MergeIntervalsPatternTests.swift
@@ -41,9 +41,53 @@ class MergeIntervalsTests: XCTestCase {
     }
 }
 
-// MARK: - Merge Intervals (Medium)
-class InstertIntervalTests: XCTestCase {
+// MARK: Similar Problem #1
+class DoIntervalsOverlapTests: XCTestCase {
     func test1() {
-        
+        let input = [Interval(1, 4), Interval(2, 5), Interval(7, 9)]
+        let result = doIntervalsOverlap(input)
+        XCTAssertTrue(result)
+    }
+    
+    func test2() {
+        let input = [Interval(10, 12), Interval(1, 2), Interval(3, 5)]
+        let result = doIntervalsOverlap(input)
+        XCTAssertFalse(result)
+    }
+    
+    func test3() {
+        let input = [Interval(1, 5), Interval(5, 9)]
+        let result = doIntervalsOverlap(input)
+        XCTAssertTrue(result)
+    }
+    
+    func test4() {
+        let input = [Interval(1, 3), Interval(4, 5)]
+        let result = doIntervalsOverlap(input)
+        XCTAssertFalse(result)
+    }
+}
+
+// MARK: - Merge Intervals (Medium)
+class InsertIntervalTests: XCTestCase {
+    func test1() {
+        let intervals = [Interval(1, 3), Interval(5, 7), Interval(8, 12)]
+        let newInterval = Interval(4, 6)
+        let result = insertInterval(newInterval, into: intervals)
+        XCTAssertEqual(result.description, "[[1, 3], [4, 7], [8, 12]]")
+    }
+    
+    func test2() {
+        let intervals = [Interval(1, 3), Interval(5, 7), Interval(8, 12)]
+        let newInterval = Interval(4, 10)
+        let result = insertInterval(newInterval, into: intervals)
+        XCTAssertEqual(result.description, "[[1, 3], [4, 12]]")
+    }
+    
+    func test3() {
+        let intervals = [Interval(2, 3), Interval(5, 7)]
+        let newInterval = Interval(1, 4)
+        let result = insertInterval(newInterval, into: intervals)
+        XCTAssertEqual(result.description, "[[1, 4], [5, 7]]")
     }
 }
