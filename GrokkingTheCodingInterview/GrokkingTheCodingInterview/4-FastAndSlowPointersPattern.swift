68e4c2db17cad2eaf80fe56b6d67a969f2968ba2
J.A. Ramirez <jaramirez1729@gmail.com>
2022-03-31 22:29:10 -0500

    Add comment.

--- GrokkingTheCodingInterview/GrokkingTheCodingInterview/4-FastAndSlowPointersPattern.swift
+++ GrokkingTheCodingInterview/GrokkingTheCodingInterview/4-FastAndSlowPointersPattern.swift
@@ -211,7 +211,7 @@ func reorder<T>(_ head: inout LinkedList<T>?) {
     }
 }
 
-// Problem Challenge 3: Cycle in a Circular Array (Hard)
+// MARK: - Problem Challenge 3: Cycle in a Circular Array (Hard)
 /*
  We are given an array containing positive and negative numbers. Suppose the array contains a number ‘M’ at a particular index. Now, if ‘M’ is positive we will move forward ‘M’ indices and if ‘M’ is negative move backwards ‘M’ indices. You should assume that the array is circular which means two things:
 
